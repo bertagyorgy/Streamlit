4638bb45ef3e7d1bd7b6ffb04cde40700f1aa712
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2023-11-23 14:16:59 +0100

    Update elso.py

--- elso.py
+++ elso.py
@@ -1,39 +1,39 @@
 import streamlit as st
  
-st.title("Calculator App using Streamlit")
+st.title("Kalkulátor App Streamlitben")
  
 # creates a horizontal line
 st.write("---")
  
 # input 1
-num1 = st.number_input(label="Enter first number")
+num1 = st.number_input(label="Első szám")
  
 # input 2
-num2 = st.number_input(label="Enter second number")
+num2 = st.number_input(label="Második szám")
  
-st.write("Operation")
+st.write("Művelet")
  
-operation = st.radio("Select an operation to perform:",
-                    ("Add", "Subtract", "Multiply", "Divide"))
+operation = st.radio("Válasszon egy műveletet:",
+                    ("Összeadás", "Kivonás", "Szorzás", "Osztás"))
  
 ans = 0
  
 def calculate():
-    if operation == "Add":
+    if operation == "Összeadás":
         ans = num1 + num2
-    elif operation == "Subtract":
+    elif operation == "Kivonás":
         ans = num1 - num2
-    elif operation == "Multiply":
+    elif operation == "Szorzás":
         ans = num1 * num2
-    elif operation=="Divide" and num2!=0:
+    elif operation=="Osztás" and num2!=0:
         ans = num1 / num2
     else:
-        st.warning("Division by 0 error. Please enter a non-zero number.")
-        ans = "Not defined"
+        st.warning("Osztás 0-val hibát jelez. Írjon egy másik számot")
+        ans = "Nincs definiálva"
  
-    st.success(f"Answer = {ans}")
+    st.success(f"Eredmény: {ans}")
  
-if st.button("Calculate result"):
+if st.button("Eredmény kikalkulásása"):
     calculate()
    
     
