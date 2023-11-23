a7eeaf3bbb54e6d1fa07c9b132b2cc6c6d508a3e
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2023-11-23 14:04:44 +0100

    Update elso.py

--- elso.py
+++ elso.py
@@ -1,37 +1,33 @@
 import streamlit as st
- 
+
 st.title("Kalkulátor app Streamlitben")
- 
-# creates a horizontal line
+
 st.write("---")
- 
-# input 1
-num1 = st.number_input(label="Első szám")
- 
-# input 2
-num2 = st.number_input(label="Második szám")
- 
-st.write("Művelet")
- 
-operation = st.radio("Milyen műveletet akarsz elvégezni?",
-                    ("Összeadás", "Kivonás", "Szorzás", "Osztás"))
- 
+
+szam1 = st.number_input(label="Első szám")
+szam2 = st.number_input(label="Második szám")
+
+st.write("Művelet kiválasztása")
+operation = st.radio("Add meg, milyen műveletet akarsz elvégezni:",
+                     ("Összeadás", "Kivonás", "Szorzás", "Osztás"))
 ans = 0
- 
-def calculate():
-    if operation == "Összeadás":
-        ans = num1 + num2
-    elif operation == "Kivonás":
-        ans = num1 - num2
-    elif operation == "Szorzás":
-        ans = num1 * num2
-    elif operation=="Osztás" and num2!=0:
-        ans = num1 / num2
-    else:
-        st.warning("A 0val osztás nem lehetséges. Próbálkozz másik számmal!")
-        ans = "Nincs definiálva"
- 
-    st.success(f"Eredmény: {ans}")
- 
-if st.button("Kalkulálás"):
-    calculate()
+
+def kalkulálás():
+  if operation == "Összeadás":
+    ans = szam1 + szam2
+  elif operation == "Kivonás":
+    ans = szam1 - szam2
+  elif operation == "Szorzás":
+    ans = szam1 * szam2
+  elif operation == "Osztás":
+    ans = szam1 / szam2
+
+  st.success(f"Eredmény: {ans}")
+ 	else:
+				st.warning("0val való osztás nem lehetséges")
+				ans = "Nincs értelmezve"
+if st.button("Eredmény kiszámolása"):
+		kalkulálás()
+   
+   
+    
