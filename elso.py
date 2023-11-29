e0753bfd62dc3f942ea1f59d3cf4d3c74f09d9ab
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2023-11-29 14:25:53 +0100

    Update elso.py

--- elso.py
+++ elso.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("A 2 hatványai")
 st.write("Görgesd a csúszkát")
 level = st.slider("2 hányadik hatványa legyen?", 1, 10)
-st.text('Selected: {}'.format(2**level))
+st.text('Kiválasztva: {}'.format(2**level))
    
     
